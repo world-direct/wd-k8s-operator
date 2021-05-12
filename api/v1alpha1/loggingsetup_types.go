40e590e575a8173ad001c5a80d9a804e010fc3e1
gprossliner <6724584+gprossliner@users.noreply.github.com>
2021-05-12 12:25:32 +0200

    operator-sdk create api --group logging --version v1alpha1 --kind LoggingSetup --resource --controller

---
+++ api/v1alpha1/loggingsetup_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// LoggingSetupSpec defines the desired state of LoggingSetup
+type LoggingSetupSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of LoggingSetup. Edit loggingsetup_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// LoggingSetupStatus defines the observed state of LoggingSetup
+type LoggingSetupStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// LoggingSetup is the Schema for the loggingsetups API
+type LoggingSetup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LoggingSetupSpec   `json:"spec,omitempty"`
+	Status LoggingSetupStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// LoggingSetupList contains a list of LoggingSetup
+type LoggingSetupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []LoggingSetup `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&LoggingSetup{}, &LoggingSetupList{})
+}
--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	loggingv1alpha1 "github.com/world-direct/wd-k8s-operator/api/v1alpha1"
+	"github.com/world-direct/wd-k8s-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(loggingv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.LoggingSetupReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("LoggingSetup"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LoggingSetup")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
