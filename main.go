a36ad6beadba92987d21aec0d24255f84adb56ee
gprossliner <6724584+gprossliner@users.noreply.github.com>
2021-05-12 13:05:48 +0200

    operator-sdk create api --group logging --version v1alpha1 --kind GraylogClusterInstance --resource --controller --namespaced=false

--- main.go
+++ main.go
@@ -86,6 +86,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LoggingSetup")
 		os.Exit(1)
 	}
+	if err = (&controllers.GraylogClusterInstanceReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("GraylogClusterInstance"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GraylogClusterInstance")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
