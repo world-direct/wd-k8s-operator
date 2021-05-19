bcea4c0b1c99483dc78875cb2a243899f083d978
gprossliner <6724584+gprossliner@users.noreply.github.com>
2021-05-19 09:31:18 +0200

    Revert "operator-sdk create api --group logging --version v1alpha1 --kind GraylogClusterInstance --resource --controller --namespaced=false"
    We will use Environment-Variables on the Controller to setup the Graylog Instance Connectiviy Parameters
    
    This reverts commit ce4752057be35953e1a435e1e4f3ea80c7c5fc90.

--- main.go
+++ main.go
@@ -86,14 +86,6 @@ func mainx() {
 		setupLog.Error(err, "unable to create controller", "controller", "LoggingSetup")
 		os.Exit(1)
 	}
-	if err = (&controllers.GraylogClusterInstanceReconciler{
-		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("GraylogClusterInstance"),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "GraylogClusterInstance")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
